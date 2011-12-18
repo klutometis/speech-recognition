c0f7e4970040a34818b966c2efb5db1459c2b6d4
Aaron Crow <dirtyvagabond@yahoo.com>
2011-12-17 20:45:56 -0800

    first compilable code

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject facts/speech-recognition "1.0.0"
   :description "Library to listen to audio input and interpret it to text."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 ]
+                 [net.sourceforge.javaflacencoder/java-flac-encoder "0.2.3"]
+                 [clj-http "0.2.5"]
+                 [org.clojure/data.json "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [lein-clojars "0.6.0"]]
-
+  :repositories {"conjars" "http://conjars.org/repo/"}
 )
